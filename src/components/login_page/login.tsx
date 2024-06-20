b553fd408d5596047a551bf2e4a1d628f7a304ad
mrabdullahsaigal1 <mrabdullahsaigal@gmail.com>
2024-06-20 15:26:17 +0500

    first commit

---
+++ src/components/login_page/login.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { useState } from "react";
+
+export default function Login() {
+  const [email,setEmail] =useState('')
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        frontend CRM
+      </div>
+    </main>
+  );
+}
