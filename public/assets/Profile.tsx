eae02c44fb392c7f3466e2653941d8941d7417ba
abdullah-kingsmen <abdullah@hypt.app>
2024-06-27 10:47:39 +0500

    assign lead  WIP

---
+++ public/assets/Profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => (
+    <svg width="20" height="26" viewBox="0 0 20 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="Group 713">
+    <path id="Vector" d="M15.5 6.11011C15.5 8.87153 13.2614 11.1101 10.5 11.1101C7.73857 11.1101 5.5 8.87153 5.5 6.11011C5.5 3.34868 7.73857 1.11011 10.5 1.11011C13.2614 1.11011 15.5 3.34868 15.5 6.11011Z" stroke="white" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path id="Vector_2" d="M10 15.1101C5.30558 15.1101 1.5 19.1395 1.5 24.1101H18.5C18.5 19.1395 14.6944 15.1101 10 15.1101Z" stroke="white" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+    </svg>
+    
+    
+);
+
+
+export default Profile;
---
+++ src/app/assign-leads/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button, Table } from "antd";
+import type { TableColumnsType } from "antd";
+import { Pagination } from "antd";
+import "./assign-leads.scss";
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
+
+const columns: TableColumnsType<DataType> = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+];
+
+const data: DataType[] = [];
+for (let i = 0; i < 10; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
+
+const App: React.FC = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const start = () => {
+    setLoading(true);
+    // ajax request after empty completing
+    setTimeout(() => {
+      setSelectedRowKeys([]);
+      setLoading(false);
+    }, 1000);
+  };
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+  const hasSelected = selectedRowKeys.length > 0;
+
+  return (
+    <div className="assign-leads">
+      <div style={{ marginBottom: 16 }}>
+        <span className="heading">Unassigned Leads</span>
+        <span style={{ marginLeft: 8 }}>
+          {hasSelected ? `Selected ${selectedRowKeys.length} items` : ""}
+        </span>
+      </div>
+      <Table
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={data}
+        pagination={false}
+      />
+      <Pagination
+        total={70}
+        defaultCurrent={1}
+        showSizeChanger={false}
+      />
+    </div>
+  );
+};
+
+export default App;
