9c0a1fa34d2137be172050ea7abb5cb2c4f8e015
abdullah-kingsmen <abdullah@hypt.app>
2024-07-02 11:26:45 +0500

    login api integrated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 
-import Login from "@/components/login_page/login";
 export default function Home() {
   return (
     <main className="">
       <div>
-         <Login/>
       </div>
     </main>
   );
