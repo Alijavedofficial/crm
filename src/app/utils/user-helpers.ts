1c4b9def75e3ea4047de2b468121ecc930e25f81
Sheraz Iqbal <sheeraz.iqbal@bentley.com>
2024-07-29 13:59:34 +0500

    testing application

--- src/app/utils/user-helpers.ts
+++ src/app/utils/user-helpers.ts
@@ -2,6 +2,7 @@ export const setToken = (token: string) => {
   localStorage.setItem("token", `Bearer ${token}`);
 };
 
+
 export const getToken = () => {
   return localStorage.getItem("token");
 };
