143f8b1ed7bf25eea23f27e7fddf70e4f34ff046
Sheraz Iqbal <sheeraz.iqbal@bentley.com>
2024-07-29 13:28:43 +0500

    fix the name issue in with auth

--- src/app/HOC/withAuth.tsx
+++ src/app/HOC/withAuth.tsx
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 
 import { useRouter } from 'next/navigation';
 import { useEffect, useState, ComponentType } from 'react';
 import { getUser } from '../utils/user-helpers';
 
-
 const withAuth = (WrappedComponent: ComponentType<any>, allowedRoles: string[]) => {
-  return (props: any) => {
+  const WithAuthComponent = (props: any) => {
     const Router = useRouter();
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
     useEffect(() => {
       const token = localStorage.getItem('token');
       if (token) {
-        const user:any = getUser();
+        const user: any = getUser();
         if (user && allowedRoles.includes(user.profile?.user.position)) {
           setIsAuthenticated(true);
         } else {
@@ -22,7 +21,7 @@ const withAuth = (WrappedComponent: ComponentType<any>, allowedRoles: string[]) 
       } else {
         Router.replace('/login');
       }
-    }, []);
+    }, [Router]); // Include Router in the dependency array
 
     if (isAuthenticated) {
       return <WrappedComponent {...props} />;
@@ -30,6 +29,10 @@ const withAuth = (WrappedComponent: ComponentType<any>, allowedRoles: string[]) 
       return null;
     }
   };
+
+  WithAuthComponent.displayName = `WithAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return WithAuthComponent;
 };
 
 export default withAuth;
--- src/app/agent-dashboard/components/backlog/backlog.tsx
+++ src/app/agent-dashboard/components/backlog/backlog.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import "./backlog.scss";
 import { Card } from "antd";
+import Image from "next/image";
 
 const Backlog: React.FC = () => {
   const dummyData = [
@@ -65,26 +66,27 @@ const Backlog: React.FC = () => {
   ];
 
   return (
-    <Card className="backlog-list">
-      <div className="heading-wrapper">
-        <span className="heading">Backlog</span>
-        <div className="count-section">
-          <span className="count">90 Leads</span>
-          <span className="description">Today's Remaining</span>
-        </div>
-      </div>
-      {dummyData.map((item, index) => {
-        return (
-          <div key={index} className="backlog-entity">
-            <span className="details-entity">{item.name}</span>
-            <span className="details-entity">{item.number}</span>
-            <span className="detail-link">
-              View Details <img src="/assets/arrow-right-orange.svg" />
-            </span>
+      <Card className="backlog-list">
+        <div className="heading-wrapper">
+          <span className="heading">Backlog</span>
+          <div className="count-section">
+            <span className="count">90 Leads</span>
+            <span className="description">Today&apos;s Remaining</span>
           </div>
-        );
-      })}
-    </Card>
+        </div>
+        {dummyData.map((item, index) => {
+          return (
+              <div key={index} className="backlog-entity">
+                <span className="details-entity">{item.name}</span>
+                <span className="details-entity">{item.number}</span>
+                <span className="detail-link">
+              View Details
+              <Image src="/assets/arrow-right-orange.svg" alt="Arrow right" width={16} height={16} />
+            </span>
+              </div>
+          );
+        })}
+      </Card>
   );
 };
 
